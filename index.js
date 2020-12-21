339742845c1589b70baac795ea6dd87047877cdd
guno-kim <64324992+guno-kim@users.noreply.github.com>
2020-12-21 19:50:38 +0900

    second

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser=require('body-parser');
+const {User}=require('./models/User');
+const config=require('./config/key')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://guno:1234@boilerplate.isshw.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
     .catch(err=>console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res)=>{
+    const user=new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+
+        return res.status(200).json({success:true});
+    })
 })
 
 app.listen(port, () => {
